3f02fef1e4e52de434f1ca2aa17a0dda4386ad4a
Brian <bjwei@uci.edu>
2018-10-05 22:51:29 -0700

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,13 @@ public class LeapYear {
         }
     }
 
+    /** Checks if year is a leap year.
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
