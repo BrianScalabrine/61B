226b0cff5f064614040f24e4db6aa7eb524cd49e
Brian Scalabrine <thesenderofmail@gmail.com>
2018-12-23 22:31:01 -0800

    completed MyHashMap optional problems

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -100,7 +101,14 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> keySet = new HashSet<>();
+        for (ArrayMap<K, V> bucket: buckets) {
+            for (K key : bucket) {
+                keySet.add(key);
+            }
+        }
+
+        return keySet;
     }
 
     /* Removes the mapping for the specified key from this map if exists.
@@ -108,7 +116,8 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * UnsupportedOperationException. */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        int hash = hash(key);
+        return buckets[hash].remove(key);
     }
 
     /* Removes the entry for the specified key only if it is currently mapped to
@@ -116,11 +125,12 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * throw an UnsupportedOperationException.*/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        int hash = hash(key);
+        return buckets[hash].remove(key, value);
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return keySet().iterator();
     }
 }
