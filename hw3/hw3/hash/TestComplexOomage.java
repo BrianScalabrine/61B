18429a5b5835f148428a9df66429135ad5a7b7bf
Brian Scalabrine <thesenderofmail@gmail.com>
2018-12-26 00:11:04 -0800

    hw3 TestComplexOomage fixes

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -35,11 +35,10 @@ public class TestComplexOomage {
     }
 
     private ComplexOomage deadlyComplexOomage() {
-        int N = StdRandom.uniform(1, 10);
+        int N = 10;
         ArrayList<Integer> params = new ArrayList<>(N);
         for (int i = 0; i < N; i += 1) {
-            //params.add(StdRandom.uniform(0, 255));
-            params.add(255);
+            params.add(StdRandom.uniform(0, 127) * 2);
         }
         return new ComplexOomage(params);
     }
