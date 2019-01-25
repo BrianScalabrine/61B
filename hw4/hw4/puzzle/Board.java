36a789b47090b3d334bdcbbac0bf238061cf9c5d
Brian Scalabrine <thesenderofmail@gmail.com>
2019-01-25 03:32:29 -0800

    finished hw4

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,146 @@
 package hw4.puzzle;
 
-public class Board {
+import edu.princeton.cs.algs4.Queue;
 
-    /** Returns the string representation of the board. 
-      * Uncomment this method. */
-    /*public String toString() {
+public class Board implements WorldState{
+    private int[][] tiles;
+    private static final int BLANK = 0;
+
+    public Board(int[][] tiles) {
+        int N = tiles.length;
+        this.tiles = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                this.tiles[i][j] = tiles[i][j];
+            }
+        }
+    }
+
+    public int tileAt(int i, int j) {
+        if (i < 0 || i >= size()) {
+            throw new java.lang.IndexOutOfBoundsException("Index out of range");
+        }
+
+        if (j < 0 || j >= size()) {
+            throw new java.lang.IndexOutOfBoundsException("Index out of range");
+        }
+
+        return tiles[i][j];
+    }
+
+    public int size() {
+        return tiles.length;
+    }
+
+    @Override
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int N = size();
+        int bug = -1;
+        int zug = -1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tileAt(i, j) == BLANK) {
+                    bug = i;
+                    zug = j;
+                }
+            }
+        }
+
+        int[][] neighborTiles = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                neighborTiles[i][j] = tileAt(i, j);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (Math.abs(-bug + i) + Math.abs(j - zug) - 1 == 0) {
+                    neighborTiles[bug][zug] = neighborTiles[i][j];
+                    neighborTiles[i][j] = BLANK;
+                    Board neighbor = new Board(neighborTiles);
+                    neighbors.enqueue(neighbor);
+                    neighborTiles[i][j] = neighborTiles[bug][zug];
+                    neighborTiles[bug][zug] = BLANK;
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
+    public int hamming() {
+        int distance = 0;
+        int N = size();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int tileAt = tileAt(i, j);
+                if (tileAt != BLANK && tileAt != (N * i) + (j + 1)) {
+                    distance++;
+                }
+            }
+        }
+
+        return distance;
+    }
+
+    public int manhattan() {
+        int distance = 0;
+        int N = size();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int value = tileAt(i, j);
+                if (value != BLANK) {
+                    int x = (int) Math.ceil((double) value / N) - 1;
+                    int y = (value - 1) % N;
+                    distance += Math.abs(i - x) + Math.abs(j - y);
+                }
+            }
+        }
+
+        return distance;
+    }
+
+    @Override
+    public int estimatedDistanceToGoal() {
+        return manhattan();
+    }
+
+    @Override
+    public boolean equals(Object y) {
+        if (this == y) {
+            return true;
+        }
+
+        if (y == null || getClass() != y.getClass()) {
+            return false;
+        }
+
+        Board other = (Board) y;
+
+        if (size() != other.size()) {
+            return false;
+        }
+
+        int N = size();
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (tileAt(i, j) != other.tileAt(i, j)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +152,5 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
-
+    }
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,68 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import java.util.Comparator;
+
+public class Solver {
+    private class SearchNode {
+        public WorldState state;
+        public SearchNode previous;
+        public int movesMade;
+        public int heuristic;
+
+        public SearchNode(WorldState state, SearchNode previous, int movesMade) {
+            this.state = state;
+            this.previous = previous;
+            this.movesMade = movesMade;
+            this.heuristic = state.estimatedDistanceToGoal();
+        }
+
+        public int priority() {
+            return movesMade + heuristic;
+        }
+    }
+
+    private int moves;
+    private Stack<WorldState> solution;
+
+    public Solver(WorldState initial) {
+        solution = new Stack<>();
+
+        MinPQ<SearchNode> fringe =
+            new MinPQ<>(Comparator.comparingInt(SearchNode::priority));
+
+        // Start node
+        fringe.insert(new SearchNode(initial,null, 0));
+
+        while (!fringe.isEmpty()) {
+            SearchNode node = fringe.delMin();
+            if (node.state.isGoal()) {
+                // Done, set up solution
+                moves = node.movesMade;
+
+                while (node != null) {
+                    solution.push(node.state);
+                    node = node.previous;
+                }
+
+                break;
+            }
+
+            for (WorldState neighbor : node.state.neighbors()) {
+                // Add neighbors to fringe, after checking not equal to grandparent
+                if (node.previous == null || !neighbor.equals(node.previous.state)) {
+                    fringe.insert(new SearchNode(neighbor, node,node.movesMade + 1));
+                }
+            }
+        }
+    }
+
+    public int moves() {
+        return moves;
+    }
+
+    public Iterable<WorldState> solution() {
+        return solution;
+    }
+}
