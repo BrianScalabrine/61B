876776181adb875e4e7c876b5a3c4ee541f5606e
Brian Scalabrine <thesenderofmail@gmail.com>
2019-02-16 21:45:30 -0800

    proj3 autocomplete trie fix

--- proj3/src/main/java/Trie.java
+++ proj3/src/main/java/Trie.java
@@ -1,7 +1,8 @@
-import java.util.LinkedList;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+//import java.util.Stack;
 
 public class Trie {
     //private static final int R = 128;
@@ -44,21 +45,21 @@ public class Trie {
         return node;
     }
 
-//    private Node putRecursive(Node node, String word, int index) {
-//        if (node == null) {
-//            node = new Node();
-//        }
-//
-//        if (index == word.length()) {
-//            node.exists = true;
-//            return node;
-//        }
-//
-//        char c = word.charAt(index);
-//        node.children.put(c, putRecursive(node.children.get(c), word, index + 1));
-//
-//        return node;
-//    }
+    private Node putRecursive(Node node, String word, int index) {
+        if (node == null) {
+            node = new Node();
+        }
+
+        if (index == word.length()) {
+            node.exists = true;
+            return node;
+        }
+
+        char c = word.charAt(index);
+        node.children.put(c, putRecursive(node.children.get(c), word, index + 1));
+
+        return node;
+    }
 
     public boolean remove(String s) {
         // TODO
@@ -66,32 +67,76 @@ public class Trie {
     }
 
     public List<String> get(String prefix) {
-        List<String> words = new LinkedList<>();
+        List<String> words = new ArrayList<>();
 
-        // Collect all words extending from this node
-        Node prefixNode = getPrefixNode(prefix);
-        collectWords(prefixNode, prefix, words);
-
+        Node prefixNode = getNode(prefix);
+        if (prefixNode != null) {
+            collectWordsRecursive(prefixNode, new StringBuilder(prefix), words);
+        }
         return words;
     }
 
-    private void collectWords(Node node, String word, List<String> words) {
-        if (node == null || words == null) {
+    private void collectWordsRecursive(Node node, StringBuilder word, List<String> words) {
+        if (node == null || words == null || word.length() == 0) {
             return;
-        } else if (node.exists) {
-            words.add(word);
         }
 
-        node.children.forEach((c, n) -> {
-            collectWords(n, word + c, words);
+        if (node.exists) {
+            words.add(word.toString());
+        }
+
+        node.children.forEach((c, child) -> {
+            collectWordsRecursive(child, new StringBuilder(word).append(c), words);
         });
     }
 
-    private Node getPrefixNode(String prefix) {
-        return getPrefixNode(root, prefix, 0);
+//    private List<String> collectWordsIterative(String prefix) {
+//        List<String> words = new ArrayList<>();
+//
+//        if (prefix.isEmpty()) {
+//            return words;
+//        }
+//
+//        Node node = getNode(prefix);
+//        if (node == null) {
+//            return words;
+//        }
+//
+//        // Depth first traversal to collect words extending prefix
+//        Stack<Node> stack = new Stack<>();
+//        stack.push(node);
+//
+//        while (!stack.empty()) {
+//            node = stack.pop();
+//            if (node != null && node.exists) {
+//
+//            }
+//        }
+//
+//        return words;
+//    }
+
+    private Node getNode(String prefix) {
+        //return getNode(root, prefix, 0);
+        return getNodeIterative(prefix);
     }
 
-    private Node getPrefixNode(Node node, String prefix, int index) {
+    private Node getNodeIterative(String prefix) {
+        if (prefix.isEmpty()) {
+            return null;
+        }
+
+        Node node = root;
+        for (char c : prefix.toCharArray()) {
+            node = node.children.get(c);
+            if (node == null) {
+                break;
+            }
+        }
+        return node;
+    }
+
+    private Node getNodeRecursive(Node node, String prefix, int index) {
         if (node == null) {
             return null;
         }
@@ -101,6 +146,6 @@ public class Trie {
             return next;
         }
 
-        return getPrefixNode(next, prefix, index + 1);
+        return getNodeRecursive(next, prefix, index + 1);
     }
 }
