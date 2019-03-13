42e70b3c2cd6b6423ebb8921c180d1cd206c0968
Brian Scalabrine <thesenderofmail@gmail.com>
2019-03-12 22:21:46 -0700

    lab 13 radix sort ascii fix

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -43,14 +43,14 @@ public class RadixSort {
      * @param index The position to sort the Strings on.
      */
     private static void sortHelperLSD(String[] asciis, int index) {
-        Queue<String>[] buckets = new Queue[R];
+        Queue<String>[] buckets = new Queue[R + 1];
         for (int i = 0; i < buckets.length; i++) {
             buckets[i] = new LinkedList<>();
         }
 
         for (String s : asciis) {
             int bucketIndex = index >= s.length() ? 0 : (int) s.charAt(index);
-            buckets[bucketIndex].add(s);
+            buckets[bucketIndex + 1].add(s);
         }
 
         int i = 0;
